7c4c67a9f18eba0205217044e26416809f7fad3d
Maruokaita <143135016+Maruokaita@users.noreply.github.com>
2024-01-07 16:23:07 +0900

    station2,3(仮)

---
+++ src/reportWebVitals.jsx
@@ -0,0 +1,14 @@
+const reportWebVitals = async (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    const webVitals = await import("web-vitals");
+    const { getCLS, getFID, getFCP, getLCP, getTTFB } = webVitals;
+
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
+  }
+};
+
+export default reportWebVitals;
